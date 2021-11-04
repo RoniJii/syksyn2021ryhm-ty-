2cb0114b79051840be01620c1ca28bdea47f6909
Valtteri <valtterinevala98@gmail.com>
2021-11-04 17:08:48 +0200

    Kortti etusivulle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -32,8 +35,20 @@ export default function App() {
       <div class="container">
       <div class="row" id="erivi">
         <div class="column col-6 float-start" id="teksti">
-          <p>Kahvila xxx on paikallinen yritys, joka tuo kauppaan laadukkaita erityiskahveja ja teetä. Tuotteet ovat huolella jalostettuja, jotta saisit parhaimman nautinnon!</p>
+          <p>Kahvila xxx on paikallinen yritys, joka tuo kauppaan laadukkaita erityiskahveja ja teetä.
+             Tuotteet ovat huolella jalostettuja, jotta saisit parhaimman nautinnon!</p>
         </div>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
         <figure>
           <img src={require("./images/kahvipussit.jpg").default} height="360" class="column col-6 float-end" alt="kahvipussi"/>
         </figure>
