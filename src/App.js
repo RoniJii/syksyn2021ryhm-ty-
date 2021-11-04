2d12adb48b58ea5af5fd188117e203f9b25d83bf
Valtteri <valtterinevala98@gmail.com>
2021-11-04 20:37:43 +0200

    Siistimistä

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -34,26 +33,15 @@ export default function App() {
       </Navbar>
       <div class="container">
       <div class="row" id="erivi">
-<<<<<<< HEAD
-        <div class="column col-5 float-start " id="teksti">
-          <p>Kahvila xxx on paikallinen yritys, joka tuo kauppaan laadukkaita erityiskahveja ja teetä. Tuotteet ovat huolella jalostettuja, jotta saisit parhaimman nautinnon!</p>
-=======
+
+
         <div class="column col-6 float-start" id="teksti">
           <p>Kahvila xxx on paikallinen yritys, joka tuo kauppaan laadukkaita erityiskahveja ja teetä.
              Tuotteet ovat huolella jalostettuja, jotta saisit parhaimman nautinnon!</p>
->>>>>>> 9c4a14fd22f13fa1b282d5e1f286aff0b7a69ac1
+
         </div>
         <div class="container-fluid">
-        <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="images.js/100px180" />
-  <Card.Body>
-    <Card.Title>Paras kahvi</Card.Title>
-    <Card.Text>
-    Nyt 50% alennuksessa Paras kahvi!
-    </Card.Text>
-    <Button variant="primary">Lisää ostoskoriin</Button>
-  </Card.Body>
-</Card>
+ 
 <p>Löydät monipuolisesta valikoimasta erilaista kahvia, teetä ja jauhamistuotteita.</p>
 <p>Tuotteiden hankinnassa pyritään tuottamaan vastuullisesti ja eettisesti. Kaikille 
             tuottajille annetaan tasavertaisesti palkkaa ja kuljetusta pyritään minimoimaan.</p>
