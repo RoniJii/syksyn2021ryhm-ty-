d129a908dd6ecc660cd0f0b7e68af4bb2ba63914
Matleena Autio <c0auma00@students.oamk.fi>
2021-11-09 12:30:29 +0200

    footerin muokkausta

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Footer from './components/footer.js';
+import Footer from './components/Footer.js';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
