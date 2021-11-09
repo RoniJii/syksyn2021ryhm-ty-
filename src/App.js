6a00333fdf9391d8664b0a335677c97275c4ca80
Valtteri <valtterinevala98@gmail.com>
2021-11-09 13:09:26 +0200

    Etusivun luontia ja ostoskorin aloittamista

--- src/App.js
+++ src/App.js
@@ -5,32 +5,27 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Footer from './components/footer.js';
-
-
+import data from "./components/back/Data/Data";
+import Header from "./components/front/Header/Header";
+import {BrowserRouter as Router} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function App() {
+
+const {productItems} = data;
+
+
+
+
+
   return (
     
       <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Kahvikauppa xxxx</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="m-auto">
-              <Nav.Link href="#home">Koti</Nav.Link>
-              <Nav.Link href="#link">Linkki</Nav.Link>
-              <NavDropdown title="Pudotushommeli" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">jtn</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">toinen jtn</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">jtn muuta</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Router>
+      <Header/>
+      </Router>
+      
 
       <div class="container">
       <div class="row" id="erivi">
