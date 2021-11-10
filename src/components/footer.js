9c716f92083ad03cdde8362f6e62f4ef9616d6b0
Matleena Autio <c0auma00@students.oamk.fi>
2021-11-10 12:58:57 +0200

    logo lisätty footeriin

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import './footer.css';
+import Logo from './logo.png';
 
 function Footer() {
   return (
     <div className='footer-container'>
-      <section class='footer-links'>
-        <div className='footer-link-wrapper row'>
-          <div class='footer-link-items'>
+      <section className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
             <figure>
-              
+              <img className= "Header-logo" src={Logo} style={{ maxHeight: '150px'}}></img>
             </figure>
           </div>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h4>Tuotteemme</h4>
             <a href='/'>Kahvit</a>
             <a href='/'>Teet</a>
             <a href='/'>Oheistuotteet</a>
           </div>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h4>Ota yhteyttä</h4>
             <p>Yhteystiedot tähän</p>
           </div>
         </div>
         <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h4>Seuraa meitä</h4>
             <a href='/'><i class='fab fa-instagram'/> Instagram</a>
             <a href='/'><i class='fab fa-facebook-f'/> Facebook</a>
@@ -32,8 +33,8 @@ function Footer() {
         </div>
       </section>
 
-      <section class='rights'>
-        <div>
+      <section className='rights col-sm-12 col-md-12'>
+        <div className="text-center">
           <p>Verkkopalveluprojekti, Ryhmä 10 &copy; 2021</p>
         </div>
       </section>
