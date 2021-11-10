10a17b9c0726d81924716fe62c76962b34381f37
Matleena Autio <c0auma00@students.oamk.fi>
2021-11-10 16:29:49 +0200

    lisätty footerin responsiivisuutta

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,25 +6,23 @@ function Footer() {
   return (
     <div className='footer-container'>
       <section className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+        <div className='footer-link-wrapper row'>
+          <div className='footer-link-items col-sm-12 col-md-4'>
             <figure>
               <img className= "Header-logo" src={Logo} style={{ maxHeight: '140px'}}></img>
             </figure>
           </div>
-          <div className='footer-link-items'>
+          <div className='footer-link-items col-sm-12 col-md-4'>
             <h4>Tuotteemme</h4>
             <a href='/'>Kahvit</a>
             <a href='/'>Teet</a>
             <a href='/'>Oheistuotteet</a>
           </div>
-          <div className='footer-link-items'>
+          <div className='footer-link-items col-sm-12 col-md-4'>
             <h4>Ota yhteyttä</h4>
             <p>Yhteystiedot tähän</p>
           </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+          <div className='footer-link-items col-sm-12 col-md-4'>
             <h4>Seuraa meitä</h4>
             <a href='/'><i class='fab fa-instagram'/> Instagram</a>
             <a href='/'><i class='fab fa-facebook-f'/> Facebook</a>
@@ -35,6 +33,7 @@ function Footer() {
 
       <section className='rights col-sm-12 col-md-12'>
         <div className="text-center">
+          <hr/>
           <p>Verkkopalveluprojekti, Ryhmä 10 &copy; 2021</p>
         </div>
       </section>
