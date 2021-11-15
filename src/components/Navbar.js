91bb005c1436ec30f362d00f72eb7e83f1cac2e6
Matleena Autio <c0auma00@students.oamk.fi>
2021-11-15 14:33:02 +0200

    index.js muokkaus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './navbar.css';
 import {Link} from "react-router-dom";
-import { Navbar, Nav, NavDropdown, Container, Form, FormControl, Button } from 'react-bootstrap';
-import Logo from './logo.png';
+// import Logo from './logo.png';
 import Cart from './Cart';
 
-export default function Navbar() {
-    render() {
-      return (
-        
-      );
-    }
+export default function Navbar({url, setCategory, cart}) {
+
+  // const [categories, setCategories] = useState([]);
+
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Web shop</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Products</a>
+              <ul className="dropdown-menu" aria-labelledby="dropdown01">
+                {/* {categories.map(category => (
+                  <li key={category.id}>
+                    <Link className="dropdown-item"
+                      to={{
+                        pathname: '/',
+                        state: {
+                          id: category.id,
+                          name: category.name,
+                        }
+                      }}
+                    >
+                    {category.name}
+                    </Link>
+                  </li>
+                ))} */}
+              </ul>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Cart cart={cart}/>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
