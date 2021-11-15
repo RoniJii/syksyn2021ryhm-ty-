0bd2564361c08e961999e0dabc1797b8e1d6d199
Matleena Autio <c0auma00@students.oamk.fi>
2021-11-15 13:42:53 +0200

    reititystä

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import React, {useEffect,useState} from 'react';
+import { Route, Switch } from 'react-router';
 import NavbarComp from './components/NavbarComp';
 import Footer from './components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel';
-import Kuva3 from './images/kuva3.jpg';
-import Kuva5 from './images/kuva5.jpg';
-import Kuva6 from './images/kuva6.jpg';
 import { useLocation } from 'react-router';
+import { Carousel } from 'react-bootstrap';
 
 
 function App() {
@@ -21,63 +19,15 @@ useEffect(()=> {
   }
 },[])
 
-
-
-
-
-
   return (
-      <>
+    <>
       <NavbarComp/>
-      <div class="container">
-      <div class="row" id="erivi">
-        <div class="column col-6 float-start" id="teksti">
-          <p>Kahvila xxx on paikallinen yritys, joka tuo kauppaan laadukkaita erityiskahveja ja teetä.
-            Tuotteet ovat huolella jalostettuja, jotta saisit parhaimman nautinnon!</p>
+        <div id="content" className="container-fluid">
+
         </div>
-      <div class="container-fluid">
 
-      <Carousel>
-  <Carousel.Item>
-    <img width={900} height={620}
-      className="d-block w-100"
-      src={Kuva6}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-    <h3>Voit nyt tilata Woltin kautta tuotteita tai hakea paikan päältä. Tervetuloa!</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img width={900} height={620}
-      className="d-block w-100"
-      src={Kuva5}
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
-    <h3> Joulun sesonkituotteet ovat nyt saapuneet! Löydät täältä jouluisia makuja!</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img width={900} height={620}
-      className="d-block w-100"
-      src={Kuva3}
-      alt="Third slide"
-    />
-
-    <Carousel.Caption style={{color: ''}}>
-     <h3>Haluatko jauhaa omat papusi? Meiltä löydät papujen jauhamiseen tarvittavat laitteet!</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-</div>
-
-
-      </div>
-      </div>
+      
       <Footer/>
-
     </>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function Home () {
     return (
-        <>
-        </>
+        <div>
+            <h3>Tuotteet</h3>
+        </div>
     );
 }
