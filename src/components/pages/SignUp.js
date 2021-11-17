1c63717ed46429b966e519ca4227f7d7c31ad80c
Henna <tormanenhenna989@gmail.com>
2021-11-17 19:24:26 +0200

    tuotteet

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../../App.css';
 import { Form } from 'react-bootstrap';
+import Kuva4 from '../images/kuva4.jpg';
 
 export default function SignUp() {
   return(
-    <>
+    <div className="sign-up">
+      <div>
+      <img width={900} height={620}
+                    className="d-block w-100"
+                    src={Kuva4}
+                    />
+      </div>
+
       <Form>
         <Form.Group className="mb-3" controlId="formGroupNimi">
           <Form.Label>Käyttäjänimi</Form.Label>
@@ -21,7 +29,7 @@ export default function SignUp() {
     </Form>
 
 
-    </>
+    </div>
 
         
   )
