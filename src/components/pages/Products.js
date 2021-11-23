dc8661bc64a858a618f7755f5e51a9d18ec7d6a4
Matleena Autio <c0auma00@students.oamk.fi>
2021-11-23 14:56:45 +0200

    Tuotteet sivulle kuva

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -40,12 +40,14 @@ const Tuotteet = [
 
 export default function Products({product,addToCart}) {
     return (
-<div style={{'padding-top': '100px'}}>
-{product?.name}
-<button className='btn btn-dark' type='button' onClick={e => addToCart(product)}>Add</button>
-<p>Our products</p>
-</div>
-
+        <>
+        <h1 className='products'>Tuotteemme</h1>
+        <div>
+            {product?.name}
+            <button className='btn btn-dark' type='button' onClick={e => addToCart(product)}>Add</button>
+            <p>Our products</p>
+        </div>
+        </>
 
     )
 }
