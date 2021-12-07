03985cab9bd691d09aa23b65dbd3423f6dc8c61b
Matleena Autio <c0auma00@students.oamk.fi>
2021-12-07 14:41:19 +0200

    kahvisivun muokkaus. tyyli näkyy nyt oikein

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -30,44 +30,44 @@ export default function Coffeecards({url, category, addToCart}) {
     return (
         <>
             <h1 className='coffee'>Kahvituotteet</h1>
-            {products.map(product => (
-                <div key={product.id}>
-                    <Link className='card_item_link'
-                    to={{
-                        pathname: '/',
-                        state: {
-                        id: product.id,
-                        name: product.name
-                        }
-                    }}>
-                    </Link> 
-                    <div className='cards'>
-                        <h3 className='teksti1'>Valikoimastamme löydät paljon erilaisia kahveja. Tutustu ja ihastastu!</h3>
-                        <div className='cards_container'>
-                            <div className='card_wrapper'>
-                                <ul className='card_items'>
-                                    <div className='card_item'>
-                                        <Link className='card_item_link'>
-                                            <figure className='card_item_pic-wrap'>
-                                                <img className='card_item_img' src={URL + 'images/' + product.image} alt={product.name}/>
-                                            </figure>
-                                            <div className='card_item_info'>
-                                                <div className='card_item_text'>
-                                                    <h5>{product.name} </h5>
-                                                    <p>{product.price} €</p>
-                                                </div>
-                                                <button className='btn' type='button' 
-                                                    onClick={e => addToCart(product)}>Lisää ostoskoriin
-                                                </button>
-                                            </div>
+            <div className='cards'>
+                <h3 className='teksti1'>Valikoimastamme löydät paljon erilaisia kahveja. Tutustu ja ihastastu!</h3>
+                    <div className='cards_container'>
+                        <div className='card_wrapper'>
+                            <ul className='card_items'>
+                                {products.map(product => (
+                                    <div key={product.id}>
+                                        <Link className='card_item_link'
+                                            to={{
+                                                pathname: '/',
+                                                state: {
+                                                id: product.id,
+                                                name: product.name
+                                                }
+                                            }}>
                                         </Link>
+                                        <div className='card_item'>
+                                            <Link className='card_item_link'>
+                                                <figure className='card_item_pic-wrap'>
+                                                    <img className='card_item_img' src={URL + 'images/' + product.image} alt={product.name}/>
+                                                </figure>
+                                                <div className='card_item_info'>
+                                                    <div className='card_item_text'>
+                                                        <h5>{product.name} </h5>
+                                                        <p>{product.price} €</p>
+                                                    </div>
+                                                    <button className='btn' type='button' 
+                                                        onClick={e => addToCart(product)}>Lisää ostoskoriin
+                                                    </button>
+                                                </div>
+                                            </Link>
+                                        </div>
                                     </div>
-                                </ul>
-                            </div>
+                                ))} 
+                            </ul>
                         </div>
-                    </div>  
-                </div>     
-            ))} 
+                    </div>
+            </div>  
         </>
     )
 
